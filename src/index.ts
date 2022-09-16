2f781d68ea95005a0610265fe77d134987572a27
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-16 04:52:17 +0300

    update: index file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express from 'express'
 import { createConnection } from '../models/connection'
-const app = express()
-const PORT = 8000
-// create database connection and sync database
-createConnection()
+import session from 'express-session'
+import * as dotenv from 'dotenv'
+import { Authentication, dumbUser } from '../config/CustomAuth'
+import { userCreationRoute } from "../routes/userCreationRoutes"
+
+dotenv.config()
+const call = async () => {
+    let user = await dumbUser()
+    console.log(user)
+}
+
+const PORT = process.env.PORT || 8000
+export const app = express()
+createConnection() // create database connection and sync database
+app.use(express.json())
+
+// app.post("/", Authentication)
+console.log("works here")
+app.use("/signup", userCreationRoute)
 
 app.listen(PORT, () => {
     console.log(`serving at port ${PORT}`)
