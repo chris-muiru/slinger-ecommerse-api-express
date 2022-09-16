ad5b9ca7043ef27777a65101473a5118dec438dc
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-16 04:50:04 +0300

    update: db success message

--- models/connection.ts
+++ models/connection.ts
@@ -55,7 +55,7 @@ export const createConnection = async () => {
     try {
         await sequelize.authenticate();
         sequelize.sync({ alter: true })
-        console.log('Connection has been established successfully.');
+        console.log('Database Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
