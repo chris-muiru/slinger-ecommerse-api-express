ccd16c47a7ef18f2ca6b78d6a9b7872229a2de66
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-16 04:50:44 +0300

    add: hook function to run before create and update of user model instance

--- models/users.ts
+++ models/users.ts
@@ -1,20 +1,43 @@
 import { sequelize } from "./connection"
+import bycrypt from "bcrypt"
 import { DataTypes, Sequelize } from "sequelize"
 export const createUser = () => {
     return sequelize.define("User", {
-        firstName: {
+        username: {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        lastName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+
         email: {
             type: DataTypes.STRING,
         },
         password: {
             type: DataTypes.STRING,
+        }
+    }, {
+        hooks: {
+            beforeCreate: (user) => {
+                const password = user.get("password")
+                if (typeof password === "string") {
+                    const salt = bycrypt.genSaltSync(10, "a")
+                    const hash = bycrypt.hashSync(password, salt)
+                    console.log(hash)
+                    user.setDataValue("password", hash)
+                }
+
+
+            },
+            beforeUpdate: (user) => {
+                const password = user.get("password")
+                if (typeof password === "string") {
+                    const salt = bycrypt.genSaltSync(10, "a")
+                    const hash = bycrypt.hashSync(password, salt)
+                    console.log(hash)
+                    user.setDataValue("password", hash)
+                }
+
+
+            }
         }
     })
 }
