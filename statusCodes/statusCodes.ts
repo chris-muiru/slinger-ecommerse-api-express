a5fef682fbd31b71d4266360906c580c2bf5c866
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-22 00:09:29 +0300

    add: status codes

---
+++ statusCodes/statusCodes.ts
@@ -0,0 +1,15 @@
+const statusCodes: {
+    HTTP_200_OK: number, HTTP_400_BAD_REQUEST: number, HTTP_401_UNAUTHORISED: number, HTTP_403_FORBIDDEN: number
+    HTTP_404_NOT_FOUND: number,
+    HTTP_500_INTERNAL_SERVER_ERROR: number
+} = {
+    HTTP_200_OK: 200,
+    HTTP_400_BAD_REQUEST: 400,
+    HTTP_401_UNAUTHORISED: 401,
+    HTTP_403_FORBIDDEN: 403,
+    HTTP_404_NOT_FOUND: 404,
+    HTTP_500_INTERNAL_SERVER_ERROR: 500
+
+
+}
+export default statusCodes
