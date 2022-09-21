28a2fae672bce69710f9a2468fb4d357d5bd1658
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-21 14:04:26 +0300

    add: customer routes

---
+++ src/routes/customerRoutes.ts
@@ -0,0 +1,25 @@
+// import express, { Router } from "express"
+// import { Request, Response } from "express"
+// const router: Router = express.Router()
+// router.route("").get(async (req: Request, res: Response) => {
+//     try {
+// 
+//         let customerQuerySet = await Customer.findAll()
+//         res.status(200).json({ msg: customerQuerySet })
+//     }
+//     catch (err) {
+//         res.status(404).json({ err: err })
+//     }
+// 
+// }
+// ).post(async (req: Request, res: Response) => {
+//     try {
+//         let customerExist = Customer.findOne({
+//             // UserId: user.id
+//         })
+//     }
+//     catch (err) {
+// 
+//     }
+// 
+// })
