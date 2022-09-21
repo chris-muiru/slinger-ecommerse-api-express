cf791142e356a259fad5c4461920f636b74d0347
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-22 00:07:35 +0300

    add: customer crud functionalities

---
+++ src/routes/customerDetail.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response, Router } from "express";
+import { AppDataSource } from "..";
+import statusCodes from "../../statusCodes/statusCodes";
+import { Customer } from "../models/users";
+
+const router: Router = express.Router()
+router.route("/:customerId").get(async (req: Request, res: Response) => {
+    const { customerId } = req.params
+    const customerRepository = AppDataSource.getRepository(Customer)
+    try {
+        const customer: Customer[] = await customerRepository.find({
+            relations: {
+                user: true
+            }, where: {
+                id: Number(customerId)
+            }
+        })
+        res.status(200).json(customer)
+    } catch (err) {
+        res.status(statusCodes.HTTP_400_BAD_REQUEST).json({ err: "bad request" })
+
+    }
+}).put(async (req: Request, res: Response) => {
+    const { customerId } = req.params
+    const customerRepository = AppDataSource.getRepository(Customer)
+    const { isCustomer } = req.body
+    try {
+        const customer: Customer | null = await customerRepository.findOne({
+            where: {
+                id: Number(customerId)
+            }
+        })
+        if (customer) {
+            customer.isCustomer = isCustomer
+            customerRepository.save(customer)
+            res.status(statusCodes.HTTP_200_OK).json(customer)
+        } else {
+            res.status(statusCodes.HTTP_404_NOT_FOUND).json({ msg: "customer doesnt exist" })
+
+        }
+    } catch (err) {
+        res.status(statusCodes.HTTP_400_BAD_REQUEST).json({ err: "bad request" })
+
+    }
+}).delete(async (req: Request, res: Response) => {
+    const { customerId } = req.params
+    const customerRepository = AppDataSource.getRepository(Customer)
+    const customer: Customer | null = await customerRepository.findOne({
+        where: {
+            id: Number(customerId)
+        }
+    })
+    if (customer) {
+        customerRepository.delete(customer)
+        res.status(statusCodes.HTTP_200_OK).json({ msg: "customer deleted successfully" })
+    } else {
+        res.status(statusCodes.HTTP_404_NOT_FOUND).json({ msg: "customer doesnt exist" })
+    }
+
+})
+
+export { router as customerDetailRoutes }
--- src/routes/customerRoutes.ts
+++ src/routes/customerRoutes.ts
@@ -1,25 +1,53 @@
-// import express, { Router } from "express"
-// import { Request, Response } from "express"
-// const router: Router = express.Router()
-// router.route("").get(async (req: Request, res: Response) => {
-//     try {
-// 
-//         let customerQuerySet = await Customer.findAll()
-//         res.status(200).json({ msg: customerQuerySet })
-//     }
-//     catch (err) {
-//         res.status(404).json({ err: err })
-//     }
-// 
-// }
-// ).post(async (req: Request, res: Response) => {
-//     try {
-//         let customerExist = Customer.findOne({
-//             // UserId: user.id
-//         })
-//     }
-//     catch (err) {
-// 
-//     }
-// 
-// })
+import express, { Request, Response, Router } from "express"
+import { AppDataSource } from ".."
+import { Customer, User } from "../models/users"
+import getCustomUser from "../utils/customUser"
+
+// TODO: prevent duplicate query inserts in both user and customer creation functions
+const router: Router = express.Router()
+router.route("").get(async (req: Request, res: Response) => {
+    try {
+        const customerRepository = AppDataSource.getRepository(Customer)
+        const customers = await customerRepository.find(
+            {
+                relations: {
+                    user: true
+                }
+            }
+        )
+        res.status(200).json(customers)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(404).json({ err: err })
+    }
+}
+)
+// create customers
+router.post("/create/:userId", async (req: Request, res: Response) => {
+    const { userId } = req.params
+    const { isCustomer } = req.body
+    const user: User | null = await getCustomUser(Number(userId))
+    const customerRepository = AppDataSource.getRepository(Customer)
+    try {
+        if (user) {
+            const customer = new Customer()
+            customer.user = user
+            customer.isCustomer = isCustomer
+            customerRepository.save(customer)
+            res.status(200).json(
+                customer
+            )
+        }
+        else {
+            res.status(400).json({ msg: `user with id ${userId} doesnt exist` })
+        }
+    }
+    catch (err) {
+        res.status(404).json({ err: err })
+
+    }
+
+})
+
+export { router as customerRoutes }
