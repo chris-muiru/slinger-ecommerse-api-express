0075e1cd244235d2d7c3d88ea34bc4ee6844296a
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-23 00:46:15 +0300

    add: update file

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,12 +1,14 @@
 import { join } from "path"
 import { DataSource } from "typeorm"
+import * as dotenv from "dotenv"
+dotenv.config()
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: "kris",
-    password: "Cm@postgres",
-    database: "ecommerseDb",
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     entities: [join(__dirname, "./models/*.{ts,js}")],
     synchronize: true,
     logging: false,
