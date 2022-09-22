a73141efb9c20bd4f2dd42ce8f39d774c64721d3
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-22 23:22:47 +0300

    update file

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -2,6 +2,5 @@ import { User } from "../models/users";
 import { AppDataSource } from "..";
 import { Repository } from "typeorm";
 // TODO: refactor all repo creation function to use just  one function
-// export const userRepository = (model) => {
-//     return AppDataSource.getRepository(User)
-// }
+
+export const userRepository: Repository<User> = AppDataSource.getRepository(User)
