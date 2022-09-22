741b4c7582e2f57be573a81554aa94692f9d9e9d
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-23 00:18:24 +0300

    refactor: move data source to datasource file

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,17 @@ import "reflect-metadata"
 import express from 'express'
 import session from 'express-session'
 import * as dotenv from 'dotenv'
-import { DataSource } from "typeorm"
+import { AppDataSource } from "./dataSource"
 import { User, Admin, Seller, Customer } from "./models/users"
 import { userRoutes } from "./routes/userRoutes"
 import { userDetailRoutes } from "./routes/userDetailRoutes"
 import { customerRoutes } from "./routes/customerRoutes"
 import { customerDetailRoutes } from "./routes/customerDetail"
-import { join } from "path"
 import morgan from "morgan"
+import { sellerRoutes } from "./routes/sellerRoutes"
+import { sellerDetailRoutes } from "./routes/sellerDetailRoutes"
 dotenv.config()
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "kris",
-    password: "Cm@postgres",
-    database: "ecommerseDb",
-    entities: [join(__dirname, "./models/*.{ts,js}")],
-    synchronize: true,
-    logging: false,
-})
+
 console.log(__dirname)
 const PORT = process.env.PORT || 8000
 export const app = express()
@@ -33,6 +24,8 @@ app.use("/users", userRoutes)
 app.use("/userDetail", userDetailRoutes)
 app.use("/customers", customerRoutes)
 app.use("/customerDetail", customerDetailRoutes)
+app.use("/sellers", sellerRoutes)
+app.use("/sellerDetail", sellerDetailRoutes)
 AppDataSource.initialize()
     .then(() => {
         AppDataSource.synchronize()
