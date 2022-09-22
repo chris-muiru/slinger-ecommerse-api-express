c15d0bbcbb502e3e1f806713740530a064d3f6f7
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-23 00:20:47 +0300

    add: seller crud functionalities

---
+++ src/routes/sellerDetailRoutes.ts
@@ -0,0 +1,62 @@
+import express, { Request, Response, Router } from "express";
+import statusCodes from "../../statusCodes/statusCodes";
+import { AppDataSource } from "../dataSource";
+import { Seller } from "../models/users";
+
+const router: Router = express.Router()
+router.route("/:sellerId").get(async (req: Request, res: Response) => {
+    const { sellerId } = req.params
+    const sellerRepository = AppDataSource.getRepository(Seller)
+    try {
+        const seller: Seller[] = await sellerRepository.find({
+            relations: {
+                user: true
+            }, where: {
+                id: Number(sellerId)
+            }
+        })
+        res.status(200).json(seller)
+    } catch (err) {
+        res.status(statusCodes.HTTP_400_BAD_REQUEST).json({ err: "bad request" })
+
+    }
+}).put(async (req: Request, res: Response) => {
+    const { sellerId } = req.params
+    const sellerRepository = AppDataSource.getRepository(Seller)
+    const { isCustomer } = req.body
+    try {
+        const seller: Seller | null = await sellerRepository.findOne({
+            where: {
+                id: Number(sellerId)
+            }
+        })
+        if (seller) {
+            seller.isSeller = isCustomer
+            sellerRepository.save(seller)
+            res.status(statusCodes.HTTP_200_OK).json(seller)
+        } else {
+            res.status(statusCodes.HTTP_404_NOT_FOUND).json({ msg: "seller doesn't exist" })
+
+        }
+    } catch (err) {
+        res.status(statusCodes.HTTP_400_BAD_REQUEST).json({ err: "bad request" })
+
+    }
+}).delete(async (req: Request, res: Response) => {
+    const { sellerId } = req.params
+    const sellerRepository = AppDataSource.getRepository(Seller)
+    const seller: Seller | null = await sellerRepository.findOne({
+        where: {
+            id: Number(sellerId)
+        }
+    })
+    if (seller) {
+        sellerRepository.delete(seller)
+        res.status(statusCodes.HTTP_200_OK).json({ msg: "seller deleted successfully" })
+    } else {
+        res.status(statusCodes.HTTP_404_NOT_FOUND).json({ msg: "seller doesnt exist" })
+    }
+
+})
+
+export { router as sellerDetailRoutes }
---
+++ src/routes/sellerRoutes.ts
@@ -0,0 +1,63 @@
+import express, { Request, Router, Response } from "express"
+import statusCodes from "../../statusCodes/statusCodes"
+import { AppDataSource } from "../dataSource"
+import { Seller, User } from "../models/users"
+import getCustomUser from "../utils/customUser"
+
+const router: Router = express.Router()
+router.route("").get(async (req: Request, res: Response) => {
+    try {
+        const sellerRepository = AppDataSource.getRepository(Seller)
+        const sellers = await sellerRepository.find(
+            {
+                relations: {
+                    user: true
+                }
+            }
+        )
+        res.status(statusCodes.HTTP_200_OK).json(sellers)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(statusCodes.HTTP_500_INTERNAL_SERVER_ERROR).json({ err: err })
+    }
+}
+)
+// create sellers
+router.post("/create/:userId", async (req: Request, res: Response) => {
+    const { userId } = req.params
+    const { isSeller } = req.body
+    const user: User | null = await getCustomUser(Number(userId))
+    const sellerRepository = AppDataSource.getRepository(Seller)
+    try {
+        if (user) {
+            const sellerExists = await sellerRepository.findOne({
+                where: {
+                    user: user
+                }
+            })
+            if (!sellerExists) {
+                const seller = new Seller()
+                seller.user = user
+                seller.isSeller = isSeller
+                sellerRepository.save(seller)
+                res.status(statusCodes.HTTP_200_OK).json(
+                    seller
+                )
+            }
+            else {
+                res.status(statusCodes.HTTP_400_BAD_REQUEST).json({ msg: "seller already exists" })
+            }
+
+        }
+        else {
+            res.status(statusCodes.HTTP_404_NOT_FOUND).json({ msg: `user with id ${userId} doesnt exist` })
+        }
+    }
+    catch (err) {
+        res.status(statusCodes.HTTP_500_INTERNAL_SERVER_ERROR).json({ err: err })
+
+    }
+
+})
+export { router as sellerRoutes }
