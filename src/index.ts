dbc7dbab1e43ecb62c6bd78407659475729c68c1
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-25 01:53:49 +0300

    add: jwt authentication

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ import { sellerRoutes } from "./routes/sellerRoutes"
 import { sellerDetailRoutes } from "./routes/sellerDetailRoutes"
 import { adminRoutes } from "./routes/adminRoutes"
 import { adminDetailRoutes } from "./routes/adminDetailRoutes"
+import { initializePassport } from "./config/passportLocal"
+import passport from "passport"
+import { checkisAuthenticated } from "./middlewares/isAuthenticatedMiddleware"
 dotenv.config()
 
 export const app = express()
@@ -21,6 +24,38 @@ const PORT = process.env.PORT || 8000
 // middlewares
 app.use(express.json())
 app.use(morgan("dev"))
+
+// authentication with passport
+initializePassport(passport)
+
+app.use(
+    //TODO: change secret to only read from SESSION_SECRET
+    session({
+        name: "slingerEccomerseAuth",
+        secret: process.env.SESSION_SECRET || "definetly not my secret...",
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 3600000000 },
+    })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.post("/auth/login", (req, res, next) => {
+    passport.authenticate("local", (err, user) => {
+        if (err) throw err
+        req.logIn(user, (loginError) => {
+            if (loginError) {
+                console.log(loginError)
+                res.status(404).send({ message: "user not found" })
+            } else if (user) {
+                res.status(200).send({ message: "authenticated" })
+            }
+        })
+    })(req, res, next)
+})
+app.use(checkisAuthenticated)
 
 // routes
 app.use("/users", userRoutes)
@@ -32,7 +67,7 @@ app.use("/sellerDetail", sellerDetailRoutes)
 app.use("/admins", adminRoutes)
 app.use("/adminDetail", adminDetailRoutes)
 
-
+// orm initialization
 AppDataSource.initialize()
     .then(() => {
         AppDataSource.synchronize()
