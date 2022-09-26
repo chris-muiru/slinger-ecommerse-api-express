3a97db69f50314eada44233e214ab1f20f52b6ea
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-27 01:32:55 +0300

    add: admin middleware

--- src/middlewares/isAdmin.ts
+++ src/middlewares/isAdmin.ts
@@ -8,24 +8,17 @@ export const isAdmin = async (req: Request, res: Response, next: NextFunction) =
     const user: User | undefined = req.session.user
     const adminRepo = AppDataSource.getRepository(Admin)
     if (user) {
-
+        //TODO: think if i need to check the isAdmin field in ADmin entity
         const adminQuery = await adminRepo.findOne({
             where: {
                 user: user
             },
-            select: {
-                id: true,
-                isAdmin: true
-            }
         })
-        console.log(adminQuery)
-        if (adminQuery && adminQuery.isAdmin) {
-            console.log(req.session.user?.admin)
-            console.log("is admin")
+
+        if (adminQuery) {
             next()
         }
         else {
-            console.log("not admin")
             res.status(statusCodes.HTTP_401_UNAUTHORISED).json({ msg: "not authorized" })
         }
     }
