fc3743c93727f01e111143e3e9cce7fa51abac2f
kris-slinger <krisdjangodeveloper@gmail.com>
2022-09-29 17:05:42 +0300

    add: request parameter used to get seller

--- src/routes/auctionProductsRoutes.ts
+++ src/routes/auctionProductsRoutes.ts
@@ -3,20 +3,23 @@ import { AppDataSource } from "../dataSource"
 import { isSeller } from "../middlewares/isSeller"
 import { AuctionedProduct } from "../models/auctionedProducts"
 import statusCodes from "../statusCodes/statusCodes"
-import customCustomer from "../utils/customCustomer"
 import customSeller from "../utils/customSeller"
 const router: Router = express.Router()
 
-router.route("").get(async (req: Request, res: Response) => {
+router.get("", async (req: Request, res: Response) => {
     const productRepo = AppDataSource.getRepository(AuctionedProduct)
     const products: AuctionedProduct[] = await productRepo.find()
 
     res.status(statusCodes.HTTP_200_OK).json(products)
 
-}).post(isSeller, async (req: Request, res: Response) => {
+})
+
+router.post("/:sellerId", isSeller, async (req: Request, res: Response) => {
+    const { sellerId } = req.params
     const { title, description, price } = req.body
     const productRepo = AppDataSource.getRepository(AuctionedProduct)
-    const seller = await customSeller(req)
+    const seller = await customSeller(Number(sellerId))
+
     if (seller) {
         const product = new AuctionedProduct()
         product.title = title
@@ -24,8 +27,7 @@ router.route("").get(async (req: Request, res: Response) => {
         product.price = price
         product.seller = seller
         await productRepo.save(product)
-
-        res.status(statusCodes.HTTP_200_OK).json({ msg: "product created" })
+        res.status(statusCodes.HTTP_200_OK).json({ msg: "product auctioned" })
     }
     else {
         res.status(statusCodes.HTTP_404_NOT_FOUND).json({ msg: "seller not found" })
